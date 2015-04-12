a1ab91ebfc06f8f07f6d776b3713e80a9fb770e7
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-04-12 20:05:27 +0530

    Added slider value events

--- ColorControl/ColorControl/MainWindow.xaml.cs
+++ ColorControl/ColorControl/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ColorControl
 {
@@ -20,9 +9,55 @@ namespace ColorControl
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Color color;
+
         public MainWindow()
         {
             InitializeComponent();
+            color = new Color();
+        }
+
+        private void Slider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            SetColorText((int) (((Slider) sender).Value), ReturnPositionIndex((Slider) sender));
+        }
+
+        ColorIndex ReturnPositionIndex(Slider slider)
+        {
+            if (slider.Name == "RedSlider")
+                return ColorIndex.Red;
+            if (slider.Name == "GreenSlider")
+                return ColorIndex.Green;
+            if (slider.Name == "BlueSlider")
+                return ColorIndex.Blue;
+            return ColorIndex.Red;
+        }
+
+        void SetColorText(int colorValue, ColorIndex index)
+        {
+            switch (index)
+            {
+                case ColorIndex.Red:
+                    RedBlock.Text = colorValue.ToString();
+                    color.R = (byte)colorValue;
+                    break;
+                case ColorIndex.Green:
+                    GreenBlock.Text = colorValue.ToString();
+                    color.G = (byte)colorValue;
+                    break;
+                case ColorIndex.Blue:
+                    BlueBlock.Text = colorValue.ToString();
+                    color.B = (byte)colorValue;
+                    break;
+            }
         }
     }
+
+    enum ColorIndex
+    {
+        Red,
+        Green,
+        Blue
+    }
+
 }
